052af07fe7e96830556c25d25614528f034a6557
Rayhanali-dev <rayhanalidev@gmail.com>
2023-03-27 14:47:41 +0600

    create header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../../images/Logo.svg';
+
+const Header = () => {
+    return (
+        <nav className='header-container'>
+            <div className="header">
+            <img src={logo} alt="" />
+            <div>
+                <ul className='menu'>
+                    <li><a href="#">Order</a></li>
+                    <li><a href="#">Order Review</a></li>
+                    <li><a href="#">Manage Inventory</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
